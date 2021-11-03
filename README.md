dc9bc84971277ba1de7e56983b890fdbd87fd752
Demon <zero2211@protonmail.com>
2021-11-02 20:22:30 -0400

    complete

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio-update-2
+
+
+# Link to deployed site
+https://michaeldigi.github.io/Portfolio-update-2/
---
+++ js/script.js
@@ -0,0 +1,15 @@
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1,
+});
+
+VanillaTilt.init(document.querySelectorAll(".features__box"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 1,
+    reverse: true
+});
+
